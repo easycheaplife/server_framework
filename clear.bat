fd0fc1b52dda20a7d8fa09628b801c048f8aa2c8
unknown <King Lee@KingLee-PC.(none)>
2014-09-01 15:54:13 +0800

    cmake for windows is ok

--- clear.bat
+++ clear.bat
@@ -4,8 +4,10 @@ echo Press any key to delete all files with ending:
 echo  *.aps *.idb *.ncp *.obj *.pch *.tmp *.sbr
 echo Visual c++/.Net junk 
 echo delete file folder use    rd  /S /Q .svn 
+echo delete hide file use    del /F /Q /S /A:H-S
 echo ----------------------------------------------------
 
-del /F /Q /S *.aps *.idb *.ncp *.obj *.pch *.sbr *.tmp *.pdb *.bsc *.ilk *.res *.ncb *.opt *.suo *.manifest *.dep *.user *.sdf *.sln *.vcxproj *.filters *.cmake CMakeCache.txt 
-
+del /F /Q /S *.aps *.idb *.ncp *.obj *.pch *.sbr *.tmp *.pdb *.bsc *.ilk *.res *.ncb *.opt *.suo *.manifest *.dep *.user *.sdf *.sln *.vcxproj *.filters *.cmake *.opensdf *.sdf 
+del /F /Q /S /A:H-S CMakeCache.txt  server_framework.v11.suo 
+rd  /S /Q CMakeFiles Debug Win32 proto.dir tinynet.dir lib
 pause
