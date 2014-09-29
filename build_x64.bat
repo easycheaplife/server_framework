882ce3d3baec31ab3402d672f36a5d2f49994222
unknown <King Lee@A02101303188.(none)>
2014-09-29 11:12:52 +0800

    set output directory and copy dll to output

--- build_x64.bat
+++ build_x64.bat
@@ -1,2 +1,5 @@
+@echo off
+::	for comment
 cmake -G "Visual Studio 11 Win64" .
 msbuild.exe ALL_BUILD.vcxproj
+xcopy  /e /y  %cd%\dep\mongoc\x64\bin %cd%\bin\Debug
--- build_x86.bat
+++ build_x86.bat
@@ -1,2 +1,5 @@
+@echo off
+::	for comment
 cmake -G "Visual Studio 11" .
 msbuild.exe ALL_BUILD.vcxproj
+xcopy  /e /y  %cd%\dep\mongoc\x86\bin %cd%\bin\Debug
