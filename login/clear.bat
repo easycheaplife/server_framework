672e92ef6ec9f0b3848049875e5e002375c30a57
unknown <King Lee@A02101303188.(none)>
2014-09-29 09:13:49 +0800

    add windows mongoc lib

---
+++ login/clear.bat
@@ -0,0 +1,12 @@
+@echo off
+echo ----------------------------------------------------
+echo Press any key to delete all files with ending:
+echo  *.aps *.idb *.ncp *.obj *.pch *.tmp *.sbr
+echo Visual c++/.Net junk 
+echo delete file folder use    rd  /S /Q .svn 
+echo delete hide file use    del /F /Q /S /A:H-S
+echo ----------------------------------------------------
+
+del /F /Q /S *.aps *.idb *.ncp *.obj *.pch *.sbr *.tmp *.pdb *.bsc *.ilk *.res *.ncb *.opt *.suo *.manifest *.dep *.user *.sdf *.sln *.vcxproj *.filters *.cmake *.opensdf *.sdf 
+rd  /S /Q CMakeFiles Debug Win32 login.dir
+pause
