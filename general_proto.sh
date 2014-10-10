a6421fe7a78d5fc788911cc6a701eb0657103176
King Lee <lijin@chukong-inc.com>
2014-10-10 17:47:10 +0800

    sync linux version and fix compiler error

--- general_proto.sh
+++ general_proto.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 export LD_LIBRARY_PATH=../easy/dep/protobuf/src/.libs
 ../easy/dep/protobuf/src/.libs/protoc -I./ --cpp_out=./ ./proto/transfer.proto
+../easy/dep/protobuf/src/.libs/protoc -I./ --cpp_out=./ ./proto/login.proto
