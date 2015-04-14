3575257116318deb90f82c00a11bc4a5a03a6e8a
King Lee <lijin157@163.com>
2015-04-14 17:20:22 +0800

    update jansson.dll servers_template.json

--- build_x64.bat
+++ build_x64.bat
@@ -4,7 +4,7 @@ cmake -G "Visual Studio 11 Win64" .
 msbuild.exe ALL_BUILD.vcxproj
 xcopy  /e /y  %cd%\dep\mongoc\x64\bin %cd%\bin\Debug
 xcopy  /e /y  %cd%\dep\mongocxx\x64\lib\mongoclient-gd.dll %cd%\bin\Debug
-xcopy  /e /y  %cd%\..\easy\dep\jansson\win32\vs2012\x64\Debug\jansson.dll %cd%\bin\Debug
+xcopy  /e /y  %cd%\dep\jansson\x64\bin\jansson.dll %cd%\bin\Debug
 xcopy  /e /y  c:\local\boost_1_53_0\lib64-msvc-11.0\boost_date_time-vc110-mt-gd-1_53.dll %cd%\bin\Debug
 xcopy  /e /y  c:\local\boost_1_53_0\lib64-msvc-11.0\boost_thread-vc110-mt-gd-1_53.dll %cd%\bin\Debug
 xcopy  /e /y  c:\local\boost_1_53_0\lib64-msvc-11.0\boost_system-vc110-mt-gd-1_53.dll %cd%\bin\Debug
