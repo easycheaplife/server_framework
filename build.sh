12f8234553e93fd7db562aa61c7909b87116050d
King Lee <lijin@chukong-inc.com>
2015-04-16 15:17:34 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -1,21 +1,5 @@
 #!/bin/bash
-echo "start compile protobuf"
-cd .. && cd easy/dep/protobuf
-./configure 
-make
-echo "protobuf compile end"
-
-echo "start compile mongo cxx"
-cd .. && cd mongo-cxx-driver
-scons all
-echo "mongo cxx compile end"
-
-echo "start compile server framework"
-cd ..
-cd ..
-cd ..
-cd server_framework
+./general_proto.sh
+LD_LIBRARY_PATH=/usr/local/lib:./dep/protobuf/x64/lib:./dep/jansson/x64/lib; export LD_LIBRARY_PATH
 cmake .
 make
-echo "server framework compile end"
-
